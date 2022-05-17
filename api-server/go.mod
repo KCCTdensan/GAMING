dd127a09e03d1273284fff86677e50eda22a4b9e
YamaD <me@dyama.net>
2022-05-17 18:37:04 +0900

    wip: 多分動く

--- api-server/go.mod
+++ api-server/go.mod
@@ -2,6 +2,26 @@ module GAMING/api-server
 
 go 1.18
 
-require github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
 
-require golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167 // indirect
+	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- api-server/main.go
+++ api-server/main.go
@@ -1,11 +1,107 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"io"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
+	"github.com/gin-gonic/gin"
 	"github.com/tarm/serial"
 )
 
+var (
+	Addr = flag.String("addr", ":8080", "listen address")
+	Dev  = flag.String("dev", "/dev/ttyUSB0", "serial device")
+	Baud = flag.Int("baud", 9600, "serial baud rate")
+)
+
+var (
+	Serial *serial.Port
+	Conf   Config
+)
+
+type Config struct {
+	Brightness int
+	Speed      int
+}
+
+func init() { flag.Parse() }
+
+func init() {
+	var err error
+
+	c := &serial.Config{
+		Name: *Dev,
+		Baud: *Baud,
+	}
+
+	Serial, err = serial.OpenPort(c)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func init() { gin.SetMode(gin.ReleaseMode) }
+
 func main() {
+
+	// serial
+	go func() {
+		defer Serial.Close()
+
+		b := make([]byte, 128)
+
+		for {
+			n, err := Serial.Read(b)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				panic(err.Error())
+			}
+
+			if a := strings.Split(string(b[:n]), ","); len(a) == 2 {
+				Conf.Brightness, _ = strconv.Atoi(a[0])
+				Conf.Speed, _ = strconv.Atoi(a[1])
+			}
+		}
+	}()
+
+	// http
+	r := gin.New()
+
+	r.POST("/brightness", func(c *gin.Context) {
+		s := c.PostForm("val")
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		Conf.Brightness = v
+		log.Print("new brightness val: ", v)
+	})
+
+	r.GET("/brightness", func(c *gin.Context) {
+		c.String(http.StatusOK, strconv.Itoa(Conf.Brightness))
+	})
+
+	r.POST("/speed", func(c *gin.Context) {
+		s := c.PostForm("val")
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		Conf.Speed = v
+		log.Print("new speed val: ", v)
+	})
+
+	r.GET("/speed", func(c *gin.Context) {
+		c.String(http.StatusOK, strconv.Itoa(Conf.Speed))
+	})
+
+	r.Run(*Addr)
 }
