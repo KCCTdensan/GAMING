744bb80ffc5922c5f240823160090951b5340587
beralus <beralus@NORIKO.local>
2022-05-21 15:51:10 +0900

    update: Serial出力をデバック時に限定

--- hardware-control/Arduino/ledribbon-better/ledribbon-better.ino
+++ hardware-control/Arduino/ledribbon-better/ledribbon-better.ino
@@ -6,7 +6,7 @@
 const byte wave[] = {0 , 1 , 2 , 3 , 5 , 8 , 11 , 15 , 20 , 25 , 30 , 36 , 43 , 49 , 56 , 64 , 72 , 80 , 88 , 97 , 105 , 114 , 123 , 132 , 141 , 150 , 158 , 167 , 175 , 183 , 191 , 199 , 206 , 212 , 219 , 225 , 230 , 235 , 240 , 244 , 247 , 250 , 252 , 253 , 254 , 255 , 254 , 253 , 252 , 250 , 247 , 244 , 240 , 235 , 230 , 225 , 219 , 212 , 206 , 199 , 191 , 183 , 175 , 167 , 158 , 150 , 141 , 132 , 123 , 114 , 105 , 97 , 88 , 80 , 72 , 64 , 56 , 49 , 43 , 36 , 30 , 25 , 20 , 15 , 11 , 8 , 5 , 3 , 2 , 1};
 
 //numberOfLEDs(In the future, It will be automized.)
-const int numOfL = 32;
+const int numOfL = 144;
 
 //ctrlData OutputPin(DO NOT TOUCH)
 const int controlPin = 5;
@@ -20,6 +20,9 @@ byte delaySeconds = 50;
 //Declaration of current corner position (to be modified)
 int nowAngle = 0;
 
+//debugging mode val
+//#define DBG
+
 //---valSetupEnd---
 
 CRGB leds[numOfL];
@@ -30,28 +33,28 @@ void setup() {
   FastLED.addLeds<WS2812B, controlPin, GRB>(leds, numOfL);
 }
 void(* resetFunc)(void) = 0;
-void(*brightFunc)(void) = rainbow_cycle;
+void(*brightFunc)(void) = rainbow_onecycle;
 
 void loop() {
-  brightFunc();
+  system_startCycle();
 }
 
 void system_command() {
   switch (Serial.read()) {
     case 'a':
-      Serial.println("a: edit Angular velocity");
+      sysdeb_disp("a: edit Angular velocity");
       anv = system_compArg();
       break;
     case 'd':
-      Serial.println("d: edit 1Cycle delaylMilliSeconds");
+      sysdeb_disp("d: edit 1Cycle delaylMilliSeconds");
       delaySeconds = system_compArg();
       break;
-    case 'c'
-        Serial.println("c: Changes to the specified light pattern.");
+    case 'c':
+        sysdeb_disp("c: Changes to the specified light pattern.");
       system_change(system_compArg());
       break;
     case 'D':
-      Serial.println("D: Displays all variables");
+      sysdeb_disp("D: Displays all variables");
       system_disp();
       break;
     case 'S':
@@ -59,17 +62,17 @@ void system_command() {
       while (Serial.available() == 0) {
         Serial.read();
       }
-      Serial.println("S: The system operation is now temporarily suspended. To resume, enter S again. In this state, no commands other than S will be accepted.");
+      sysdeb_disp("S: The system operation is now temporarily suspended. To resume, enter S again. In this state, no commands other than S will be accepted.");
       while (Serial.read() != 'S') {}
-      Serial.println("S: Resume system operation.\n");
+      sysdeb_disp("S: Resume system operation.\n");
       break;
     case 'R':
-      Serial.println("R: System reset.\n");
+      sysdeb_disp("R: System reset.\n");
       Serial.flush();
       resetFunc();
       break;
     default:
-      Serial.println("Command not found.\n");
+      sysdeb_disp("Command not found.\n");
       break;
   }
   return;
@@ -85,32 +88,32 @@ int system_compArg() {
   }
   Serial.read();//null code removal
 
-  Serial.print("Set val: ");
-  Serial.println(o);
-  Serial.println();
+  sysdeb_disp("Set val: ");
+  sysdeb_dispval(o);
+  sysdeb_disp("");
   return o;
 }
 
 void system_change(byte type) {
   switch (type) {
     case 0:
-      brightFunc = rainbow_cycle;
-      Serial.println("c: The light pattern is set to GAMING.");
+      brightFunc = rainbow_onecycle;
+      sysdeb_disp("c: The light pattern is set to GAMING.");
       break;
     default:
-      Serial.println("c: Error ! Non-existent specification code.");
+      sysdeb_disp("c: Error ! Non-existent specification code.");
   }
-  Serial.println();
+  sysdeb_disp("");
 
   return;
 }
 
 void system_disp() {
-  Serial.print("Angular velocity val: ");
-  Serial.println(anv);
-  Serial.print("1Cycle delayMilliSeconds val: ");
-  Serial.println(delaySeconds);
-  Serial.println("All vals are displayed.\n");
+  sysdeb_disp("Angular velocity val: ");
+  sysdeb_dispval(anv);
+  sysdeb_disp("1Cycle delayMilliSeconds val: ");
+  sysdeb_dispval(delaySeconds);
+  sysdeb_disp("All vals are displayed.\n");
 }
 
 void system_cutoff() {
@@ -126,6 +129,18 @@ void system_cutoff() {
 void system_startCycle() {
   if (Serial.available() > 0)system_command();
   brightFunc();
+}
+
+void sysdeb_disp(String str){
+  #ifdef DEB
+  Serial.println(str);
+  #endif
+}
+
+void sysdeb_dispval(int val){
+  #ifdef DEB
+  Serial.print(val);
+  #endif
 }
 
 void rainbow_onecycle() {
