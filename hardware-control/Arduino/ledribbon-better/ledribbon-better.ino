167a26a80918d6fb9f6faa1938bf625976a13a9a
beralus <beralus@NORIKO.local>
2022-05-21 16:42:31 +0900

    add: 新しい光り方を作成

--- hardware-control/Arduino/ledribbon-better/ledribbon-better.ino
+++ hardware-control/Arduino/ledribbon-better/ledribbon-better.ino
@@ -3,7 +3,7 @@
 //---valSetup(PLS CHECK val)---
 
 //Array definition (-cos wave divided into 90 discrete amplitudes from 0 to 255)
-const byte wave[] = {0 , 1 , 2 , 3 , 5 , 8 , 11 , 15 , 20 , 25 , 30 , 36 , 43 , 49 , 56 , 64 , 72 , 80 , 88 , 97 , 105 , 114 , 123 , 132 , 141 , 150 , 158 , 167 , 175 , 183 , 191 , 199 , 206 , 212 , 219 , 225 , 230 , 235 , 240 , 244 , 247 , 250 , 252 , 253 , 254 , 255 , 254 , 253 , 252 , 250 , 247 , 244 , 240 , 235 , 230 , 225 , 219 , 212 , 206 , 199 , 191 , 183 , 175 , 167 , 158 , 150 , 141 , 132 , 123 , 114 , 105 , 97 , 88 , 80 , 72 , 64 , 56 , 49 , 43 , 36 , 30 , 25 , 20 , 15 , 11 , 8 , 5 , 3 , 2 , 1};
+const byte wave[90] = {0 , 1 , 2 , 3 , 5 , 8 , 11 , 15 , 20 , 25 , 30 , 36 , 43 , 49 , 56 , 64 , 72 , 80 , 88 , 97 , 105 , 114 , 123 , 132 , 141 , 150 , 158 , 167 , 175 , 183 , 191 , 199 , 206 , 212 , 219 , 225 , 230 , 235 , 240 , 244 , 247 , 250 , 252 , 253 , 254 , 255 , 254 , 253 , 252 , 250 , 247 , 244 , 240 , 235 , 230 , 225 , 219 , 212 , 206 , 199 , 191 , 183 , 175 , 167 , 158 , 150 , 141 , 132 , 123 , 114 , 105 , 97 , 88 , 80 , 72 , 64 , 56 , 49 , 43 , 36 , 30 , 25 , 20 , 15 , 11 , 8 , 5 , 3 , 2 , 1};
 
 //numberOfLEDs(In the future, It will be automized.)
 const int numOfL = 144;
@@ -50,7 +50,7 @@ void system_command() {
       delaySeconds = system_compArg();
       break;
     case 'c':
-        sysdeb_disp("c: Changes to the specified light pattern.");
+      sysdeb_disp("c: Changes to the specified light pattern.");
       system_change(system_compArg());
       break;
     case 'D':
@@ -100,6 +100,26 @@ void system_change(byte type) {
       brightFunc = rainbow_onecycle;
       sysdeb_disp("c: The light pattern is set to GAMING.");
       break;
+    case 1:
+      brightFunc = white_onecycle;
+      sysdeb_disp("c: The light pattern is set to white.");
+      break;
+    case 2:
+      brightFunc = daylight_onecycle;
+      sysdeb_disp("c: The light pattern is set to daylight white.");
+      break;
+    case 3:
+      brightFunc = subdued_onecycle;
+      sysdeb_disp("c: The light pattern is set to subdued white.");
+      break;
+    case 4:
+      brightFunc = sepia_onecycle;
+      sysdeb_disp("c: The light pattern is set to sepia.");
+      break;
+    case 5:
+      brightFunc = redwave_onecycle;
+      sysdeb_disp("c: The light pattern is set to REDWAVE.");
+      break;
     default:
       sysdeb_disp("c: Error ! Non-existent specification code.");
   }
@@ -109,18 +129,18 @@ void system_change(byte type) {
 }
 
 void system_disp() {
-  #ifdef DEB
+#ifdef DEB
   sysdeb_disp("Angular velocity val: ");
   sysdeb_dispval(anv);
   sysdeb_disp("1Cycle delayMilliSeconds val: ");
   sysdeb_dispval(delaySeconds);
   sysdeb_disp("All vals are displayed.\n");
-  #else
+#else
   Serial.print(anv);
   Serial.print(" ");
   Serial.print(delaySeconds);
   Serial.println();
-  #endif
+#endif
 }
 
 void system_cutoff() {
@@ -132,30 +152,33 @@ void system_cutoff() {
   FastLED.show();
 }
 
+void system_shiftColors() {
+  for (int i = numOfL - 1; i > 0; i--) {
+    leds[i].r = leds[i - 1].r;
+    leds[i].g = leds[i - 1].g;
+    leds[i].b = leds[i - 1].b;
+  }
+}
 
 void system_startCycle() {
   if (Serial.available() > 0)system_command();
   brightFunc();
 }
 
-void sysdeb_disp(String str){
-  #ifdef DEB
+void sysdeb_disp(String str) {
+#ifdef DEB
   Serial.println(str);
-  #endif
+#endif
 }
 
-void sysdeb_dispval(int val){
-  #ifdef DEB
+void sysdeb_dispval(int val) {
+#ifdef DEB
   Serial.print(val);
-  #endif
+#endif
 }
 
 void rainbow_onecycle() {
-  for (int i = numOfL - 1; i > 0; i--) {
-    leds[i].r = leds[i - 1].r;
-    leds[i].g = leds[i - 1].g;
-    leds[i].b = leds[i - 1].b;
-  }
+  system_shiftColors();
   if (nowAngle < 45) {
     leds[0].r = wave[nowAngle % 45 + 45];
     leds[0].g = wave[nowAngle % 45];
@@ -171,6 +194,59 @@ void rainbow_onecycle() {
   }
   nowAngle += anv;
   if (nowAngle > 134)nowAngle %= 135;
+  delay(delaySeconds);
+
+  FastLED.show();
+  return;
+}
+
+void white_onecycle() {
+  system_shiftColors();
+  leds[0].r = 255;
+  leds[0].g = 255;
+  leds[0].b = 255;
+
+  FastLED.show();
+  return;
+}
+
+void daylight_onecycle() {
+  system_shiftColors();
+  leds[0].r = 255;
+  leds[0].g = 180;
+  leds[0].b = 100;
+
+  FastLED.show();
+  return;
+}
+
+void subdued_onecycle() {
+  system_shiftColors();
+  leds[0].r = 255;
+  leds[0].g = 100;
+  leds[0].b = 25;
+
+  FastLED.show();
+  return;
+}
+
+void sepia_onecycle() {
+  system_shiftColors();
+  leds[0].r = 255;
+  leds[0].g = 50;
+  leds[0].b = 5;
+
+  FastLED.show();
+  return;
+}
+
+void redwave_onecycle() {
+  system_shiftColors();
+  if (nowAngle > 89)nowAngle %= 90;
+  leds[0].r = wave[nowAngle];
+  leds[0].g = wave[0];
+  leds[0].b = wave[0];
+  nowAngle += anv;
   delay(delaySeconds);
 
   FastLED.show();
