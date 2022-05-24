6b149f3863841678562dc2503b759ca83f5b97db
YamaD <me@dyama.net>
2022-05-24 14:03:50 +0900

    updated APIs

--- api-server/main.go
+++ api-server/main.go
@@ -22,6 +22,7 @@ var (
 type Config struct {
 	Angle int
 	Delay int
+	Type int
 }
 
 func listenSerial(s *serial.Port, c chan string) {
@@ -60,7 +61,7 @@ func main() {
 	go func() {
 		// 仮
 		for s := range ch {
-			if a := strings.Split(s, ","); len(a) == 2 {
+			if a := strings.Split(s[:len(s)-2], ","); len(a) == 2 {
 				v0, err := strconv.Atoi(a[0])
 				if err != nil {
 					continue
@@ -69,13 +70,34 @@ func main() {
 				if err != nil {
 					continue
 				}
-				conf = Config{v0, v1}
+				conf.Angle = v0
+				conf.Delay = v1
 			}
 		}
 	}()
+	s.Write([]byte("D"))
 
 	// http
 	r := gin.New()
+
+	r.GET("/angle", func(c *gin.Context) {
+		c.String(http.StatusOK, strconv.Itoa(conf.Angle))
+	})
+	r.GET("/type", func(c *gin.Context) {
+		c.String(http.StatusOK, strconv.Itoa(conf.Type))
+	})
+	r.GET("/delay", func(c *gin.Context) {
+		c.String(http.StatusOK, strconv.Itoa(conf.Delay))
+	})
+
+	r.POST("/suspend", func(c *gin.Context) {
+		s.Write([]byte("S"))
+		log.Print("Suspend")
+	})
+	r.POST("/reset", func(c *gin.Context) {
+		s.Write([]byte("R"))
+		log.Print("Reset")
+	})
 
 	r.POST("/angle", func(c *gin.Context) {
 		v := c.PostForm("val")
@@ -86,13 +108,8 @@ func main() {
 		}
 		conf.Angle = n
 		s.Write([]byte(fmt.Sprintf("a %d", n)))
-		log.Print("new brightness val: ", n)
+		log.Printf("Angle = %d", n)
 	})
-
-	r.GET("/angle", func(c *gin.Context) {
-		c.String(http.StatusOK, strconv.Itoa(conf.Angle))
-	})
-
 	r.POST("/delay", func(c *gin.Context) {
 		v := c.PostForm("val")
 		n, err := strconv.Atoi(v)
@@ -102,15 +119,18 @@ func main() {
 		}
 		conf.Delay = n
 		s.Write([]byte(fmt.Sprintf("d %d", n)))
-		log.Print("new delay val: ", n)
+		log.Printf("Delay = %d", n)
 	})
-
-	r.GET("/delay", func(c *gin.Context) {
-		c.String(http.StatusOK, strconv.Itoa(conf.Delay))
-	})
-
-	r.POST("/reset", func(c *gin.Context) {
-		s.Write([]byte("R"))
+	r.POST("/type", func(c *gin.Context) {
+		v := c.PostForm("val")
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		conf.Type = n
+		s.Write([]byte(fmt.Sprintf("c %d", n)))
+		log.Printf("Type = %d", n)
 	})
 
 	r.Run(*Addr)
