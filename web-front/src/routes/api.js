1b84a09007f2d164255baed1f9dd2027afe0cf28
YamaD <me@dyama.net>
2022-05-27 18:02:08 +0900

    update web

--- web-front/src/routes/api.js
+++ web-front/src/routes/api.js
@@ -1,8 +1,8 @@
 export const get = async () => {
   const [angle, delay, type] = await Promise.all([
-    fetch(`${process.env.API_BASE}/angle`).then(r => r.text()),
-    fetch(`${process.env.API_BASE}/delay`).then(r => r.text()),
-    fetch(`${process.env.API_BASE}/type`).then(r => r.text()),
+    fetch(`http://${process.env.API_HOST}/angle`).then(r => r.text()),
+    fetch(`http://${process.env.API_HOST}/delay`).then(r => r.text()),
+    fetch(`http://${process.env.API_HOST}/type`).then(r => r.text()),
   ])
 
   return {
@@ -14,5 +14,30 @@ export const get = async () => {
   }
 }
 
-export const post = async () => {
+export const post = async ({ request }) => {
+  const val = await request.json()
+  const [angle, delay, type] = [val.angle, val.delay, val.type]
+    .map(v => new URLSearchParams({ val: `${v}` }))
+  const res = await Promise.all([
+    fetch(`http://${process.env.API_HOST}/angle`, {
+      method: "POST",
+      body: angle,
+    }),
+    fetch(`http://${process.env.API_HOST}/delay`, {
+      method: "POST",
+      body: delay,
+    }),
+    fetch(`http://${process.env.API_HOST}/type`, {
+      method: "POST",
+      body: type,
+    }),
+  ])
+  const ratelimit = res.some(r => r.status === 429)
+  return {
+    status: ratelimit
+      ? 429
+      : res.some(r => !r.ok)
+      ? 500
+      : 200,
+  }
 }
--- web-front/src/routes/index.svelte
+++ web-front/src/routes/index.svelte
@@ -8,18 +8,34 @@
   let type = 0
   let angle = 0
   let delay = 0
+  $: guruguru = false
 
-  const init = fetch("/api").then(async r => { if(r.ok) {
-    ;({ type, angle, delay } = await r.json())
-  }})
+  const sleep = s => new Promise(r => setTimeout(r, s * 1000))
+
+  const get = async () => ({ type, angle, delay } = await fetch("/api").then(r => r.json()))
 
   const update = async () => {
+    guruguru = true
+    const res = await fetch("/api", {
+      method: "POST",
+      body: JSON.stringify({ type, angle, delay }),
+    })
+    if (res.status === 429) {
+      await sleep(1)
+      return update()
+    }
+    get()
+    guruguru = false
   }
 </script>
 
 <h1>GAMING</h1>
 
-{#await init}
+{#if guruguru}
+  guruguru...
+{/if}
+
+{#await get()}
   <p>loading...</p>
 {:then}
   <h2>Type</h2>
