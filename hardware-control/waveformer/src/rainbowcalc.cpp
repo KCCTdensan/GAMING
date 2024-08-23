657aa771de299dd35f0a966dceaa8510420f9078
jnc-explosion <himajin8634@gmail.com>
2024-08-23 14:11:06 +0900

    fixfix

--- hardware-control/waveformer/src/rainbowcalc.cpp
+++ hardware-control/waveformer/src/rainbowcalc.cpp
@@ -36,7 +36,7 @@ void rainbowcalc( int s, int m, bool expon ) {
 	for( int theta = 0; theta <= rng; theta++ ) {
 		int waveform;
 		long double cosvalue = cos( 2.0 * M_PI * ( (long double) theta / rng ) );
-		if( expon ) waveform = ( m * ( ( exp( cosvalue - 1.0 ) ) + ( exp( -2.0 ) * cosvalue ) ) );
+		if( expon ) waveform = ( m * ( ( exp( cosvalue - 1.0 ) ) + ( exp( -2.0 ) * ( cosvalue - 1.0 ) / 2.0 ) ) );
 		else waveform = ( m / 2 ) - ( ( m / 2.0 - 1 ) * cosvalue );
 		nu.push_back( waveform );
 	}
